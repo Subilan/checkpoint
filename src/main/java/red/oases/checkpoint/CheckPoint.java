43000e8d6b3f5ac4020a9722cc907da777d599b1
Subilan <suyuanliang1995@gmail.com>
2023-08-06 17:35:26 +0800

    init

---
+++ src/main/java/red/oases/checkpoint/CheckPoint.java
@@ -0,0 +1,17 @@
+package red.oases.checkpoint;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CheckPoint extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
