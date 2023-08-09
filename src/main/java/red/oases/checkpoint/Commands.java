80daca73545f0d5e4aa43f7150b8b15119e6cd6e
Subilan <suyuanliang1995@gmail.com>
2023-08-09 23:12:58 +0800

    copy

--- src/main/java/red/oases/checkpoint/Commands.java
+++ src/main/java/red/oases/checkpoint/Commands.java
@@ -65,6 +65,54 @@ public class Commands implements CommandExecutor {
             }
 
             switch (args[0]) {
+                case "copy", "cp" -> {
+                    if (args.length < 3) {
+                        LogUtil.send("参数不足：/cpt copy <from-track.number> <to-track.number> [force?]", sender);
+                        return true;
+                    }
+
+                    var from = args[1];
+                    var to = args[2];
+                    var force = false;
+
+                    if (args.length >= 4) {
+                        if (args[3].equals("true")) {
+                            force = true;
+                        }
+
+                        if (!args[3].equals("true") && !args[3].equals("false")) {
+                            LogUtil.send("参数 force? 只能为 true 或者 false。", sender);
+                            return true;
+                        }
+                    }
+
+                    var section = Files.selections.getConfigurationSection("data");
+
+                    if (section == null) {
+                        LogUtil.send("目前还没有任何检查点。", sender);
+                        return true;
+                    }
+
+                    var fromList = section.getStringList(from);
+                    if (fromList.isEmpty()) {
+                        LogUtil.send(from + " 对应的路径点不存在。", sender);
+                        return true;
+                    }
+
+                    var toList = section.getStringList(to);
+
+                    if (!force && !toList.isEmpty()) {
+                        LogUtil.send(to + " 对应的路径点已经有值。如果需要覆盖，请在指令末尾加上 true。", sender);
+                        return true;
+                    }
+
+                    section.set(to, fromList);
+                    Files.saveSelections();
+
+                    LogUtil.send("成功将 " + from + " 复制到 " + to + "。", sender);
+                    return true;
+                }
+
                 case "move" -> {
                     if (args.length < 4) {
                         LogUtil.send("参数不足：/cpt move <from-track> <to-track> <numbers>", sender);
