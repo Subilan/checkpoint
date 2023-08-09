20c79358a9f13ed1ede978db9867943baf58d8bf
Subilan <suyuanliang1995@gmail.com>
2023-08-09 23:23:46 +0800

    tab

--- src/main/java/red/oases/checkpoint/Tab.java
+++ src/main/java/red/oases/checkpoint/Tab.java
@@ -3,14 +3,44 @@ package red.oases.checkpoint;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Tab implements TabCompleter {
+    public Set<String> getTracks() {
+        var section = Files.selections.getConfigurationSection("data");
+
+        if (section == null) {
+            return new HashSet<>();
+        } else {
+            return section.getKeys(false);
+        }
+    }
+
+    public List<String> getAllPaths() {
+        var result = new ArrayList<String>();
+        var section = Files.selections.getConfigurationSection("data");
+
+        if (section == null) {
+            return List.of();
+        }
+
+        for (String key1 : getTracks()) {
+            var selectionKeys = Objects
+                    .requireNonNull(section.getConfigurationSection(key1))
+                    .getKeys(false);
+
+            for (String key2 : selectionKeys) {
+                result.add(key1 + "." + key2);
+            }
+        }
+
+        return result;
+    }
+
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!label.equals("checkpoint") && !label.equals("cpt")) return null;
@@ -22,42 +52,45 @@ public class Tab implements TabCompleter {
                     "remove",
                     "build",
                     "list",
-                    "about"
+                    "about",
+                    "copy",
+                    "cp",
+                    "move"
             );
         }
 
         if (args.length == 2) {
             switch (args[0]) {
-                case "remove" -> {
-                    var result = new ArrayList<String>();
-
-                    var section = Files.selections.getConfigurationSection("data");
-
-                    if (section == null) {
-                        return null;
-                    }
-
-                    var namespaceKeys = section.getKeys(false);
-
-                    for (String key1 : namespaceKeys) {
-                        var selectionKeys = Objects
-                                .requireNonNull(section.getConfigurationSection(key1))
-                                .getKeys(false);
-
-                        for (String key2 : selectionKeys) {
-                            result.add(key1 + "." + key2);
-                        }
-                    }
-
-                    return result;
+                case "remove", "copy", "cp" -> {
+                    return getAllPaths();
                 }
 
-                case "list" -> {
-                    var namespaces = Files.selections.getConfigurationSection("data");
-                    if (namespaces == null) {
-                        return null;
-                    }
-                    return new ArrayList<>(namespaces.getKeys(false));
+                case "list", "move" -> {
+                    return new ArrayList<>(getTracks());
+                }
+            }
+        }
+
+        if (args.length == 3) {
+            switch (args[0]) {
+                case "copy", "cp" -> {
+                    return List.of("<track.number>");
+                }
+
+                case "move" -> {
+                    return List.of("<any-empty-track-name>");
+                }
+            }
+        }
+
+        if (args.length == 4) {
+            switch (args[0]) {
+                case "copy", "cp" -> {
+                    return List.of("true", "false");
+                }
+
+                case "move" -> {
+                    return List.of("<n1,n2,n3,...>");
                 }
             }
         }
