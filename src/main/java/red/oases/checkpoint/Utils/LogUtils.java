c5ba677b8deda6fb503bcca4a4ca8f35460e3d38
Subilan <suyuanliang1995@gmail.com>
2023-08-16 12:57:44 +0800

    refactor: LogUtils

--- src/main/java/red/oases/checkpoint/Utils/LogUtils.java
+++ src/main/java/red/oases/checkpoint/Utils/LogUtils.java
@@ -2,6 +2,7 @@ package red.oases.checkpoint.Utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
@@ -14,21 +15,42 @@ public final class LogUtils {
         LogUtils.logger = logger;
     }
 
-    public static void send(String text, CommandSender p) {
-        p.sendMessage(Component
+    public static Component getDefaultPrefix() {
+        return Component
                 .text("[")
-                .append(Component
-                        .text("cpt")
-                        .color(NamedTextColor.YELLOW))
-                .append(Component.text("] "))
-                .append(Component.text(text)));
+                .append(t("cpt", NamedTextColor.YELLOW))
+                .append(t("] "));
+    }
+
+    public static Component t(String str) {
+        return Component.text(str);
+    }
+
+    public static Component t(String str, TextColor color) {
+        return Component.text(str).color(color);
+    }
+
+    public static Component getPrefixedDefaultPrefix(Component prefix) {
+        return getDefaultPrefix()
+                .append(prefix);
+    }
+
+    public static void send(String text, CommandSender p) {
+        p.sendMessage(getDefaultPrefix().append(t(text)));
+    }
+
+    public static void sendError(String text, CommandSender p) {
+        p.sendMessage(getPrefixedDefaultPrefix(t("错误", NamedTextColor.RED).append(t(text))));
+    }
+
+    public static void sendSuccess(String text, CommandSender p) {
+        p.sendMessage(getPrefixedDefaultPrefix(t("成功", NamedTextColor.GREEN).append(t(text))));
     }
 
     public static void sendWithoutPrefix(String text, CommandSender p) {
-        p.sendMessage(Component
-                .text(text));
+        p.sendMessage(t(text));
     }
-    
+
     @SuppressWarnings("deprecation")
     public static void sendLegacy(String text, CommandSender p) {
         p.sendMessage(ChatColor.translateAlternateColorCodes('&', text));
