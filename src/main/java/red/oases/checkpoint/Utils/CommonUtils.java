0ca7a3ea3d7b3d66d4c53930945bfd5261c40159
Subilan <suyuanliang1995@gmail.com>
2023-08-17 23:18:37 +0800

    refactor: remove deprecated alias

--- src/main/java/red/oases/checkpoint/Utils/CommonUtils.java
+++ src/main/java/red/oases/checkpoint/Utils/CommonUtils.java
@@ -40,31 +40,6 @@ public class CommonUtils {
 
 
     /**
-     * 获得指向指定 path 的别名
-     *
-     * @param path 指定 path，不带 data. 前缀
-     * @return 指定别名。如果不存在，返回 ""
-     */
-    public static String getAliasByPath(String path) {
-        var result = new AtomicReference<>("");
-
-        var aliasSection = FileUtils.tracks.getConfigurationSection("aliases");
-
-        if (aliasSection != null) {
-            var map = aliasSection.getValues(false);
-            map.forEach((key, value) -> {
-                if (value.equals("data." + path)) result.set(key);
-            });
-        }
-
-        return result.get();
-    }
-
-    public static @Nullable String getPathByAlias(String alias) {
-        return FileUtils.tracks.getString(String.format("aliases.%s", alias));
-    }
-
-    /**
      * 转换对应字符串为非负整数。如果转换失败，返回 0。
      *
      * @param target 待转换的字符串
