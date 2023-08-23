618a6de9db72e9b34334ee3aa3a719682e40e754
Subilan <suyuanliang1995@gmail.com>
2023-08-23 12:40:39 +0800

    fix: list display chaos

--- src/main/java/red/oases/checkpoint/Commands/CommandList.java
+++ src/main/java/red/oases/checkpoint/Commands/CommandList.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.CommandSender;
 import red.oases.checkpoint.Objects.DisplayList;
+import red.oases.checkpoint.Objects.Track;
 import red.oases.checkpoint.Utils.FileUtils;
 import red.oases.checkpoint.Utils.LogUtils;
 import red.oases.checkpoint.Utils.CommonUtils;
@@ -22,9 +23,9 @@ public class CommandList extends Command {
             return true;
         }
 
-        var track = args[1];
+        var tr = args[1];
         var page = 1;
-        var section = FileUtils.tracks.getConfigurationSection("data." + track);
+        var section = FileUtils.tracks.getConfigurationSection("data." + tr);
 
         if (args.length == 3) {
             page = CommonUtils.mustPositive(args[2]);
@@ -35,7 +36,7 @@ public class CommandList extends Command {
         }
 
         if (section == null) {
-            LogUtils.send(String.format("找不到赛道 %s", track), sender);
+            LogUtils.send(String.format("找不到赛道 %s", tr), sender);
             return true;
         }
 
@@ -44,26 +45,29 @@ public class CommandList extends Command {
                 10,
                 keys.size(),
                 sender,
-                DisplayList.getTitle(track, "下的所有路径点")
+                DisplayList.getTitle(tr, "下的所有路径点")
         );
+        var track = new Track(tr);
 
         list.sendPage(page, i -> {
-            var k = keys.get(i);
-            var targetSection = FileUtils.tracks.getConfigurationSection(
-                    String.format("data.%s.%s", track, k)
-            );
-            if (targetSection == null) return null;
-            var pos1 = targetSection.getIntegerList("pos1");
-            var pos2 = targetSection.getIntegerList("pos2");
-            var creator = targetSection.getString("creator");
+            var pt = track.getPoints().get(i);
+            var pos1 = pt.getFirstPosition();
+            var pos2 = pt.getSecondPosition();
+            var creator = pt.getCreator();
+            var date = CommonUtils.formatDate(pt.getCreatedAt());
             return Component.empty()
+                    .append(Component.text("[").color(NamedTextColor.GRAY))
+                    .append(Component.text(i + 1).color(NamedTextColor.GREEN))
+                    .append(Component.text("]").color(NamedTextColor.GRAY))
+                    .appendSpace()
                     .append(Component.text("(%s, %s, %s)".formatted(pos1.get(0), pos1.get(1), pos1.get(2))).color(NamedTextColor.YELLOW))
                     .appendSpace()
                     .append(Component.text("-").color(NamedTextColor.GRAY))
                     .appendSpace()
                     .append(Component.text("(%s, %s, %s)".formatted(pos2.get(0), pos2.get(1), pos2.get(2))).color(NamedTextColor.YELLOW))
                     .appendSpace()
-                    .append(Component.text("由 %s 创建".formatted(creator)).color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC));
+                    .append(Component.text("由 %s 创建于 %s".formatted(creator, date)).color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC))
+                    .appendNewline();
         });
 
         return true;
