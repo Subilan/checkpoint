a9690932921136ad987053c6c0733d008e934f3e
Subilan <suyuanliang1995@gmail.com>
2023-08-23 17:12:52 +0800

    refactor: join-switch logic

--- src/main/java/red/oases/checkpoint/Commands/CommandJoin.java
+++ src/main/java/red/oases/checkpoint/Commands/CommandJoin.java
@@ -4,9 +4,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import red.oases.checkpoint.Extra.Annotations.DisableConsole;
 import red.oases.checkpoint.Extra.Annotations.PermissionLevel;
-import red.oases.checkpoint.Objects.Campaign;
+import red.oases.checkpoint.Objects.Config;
 import red.oases.checkpoint.Utils.LogUtils;
 import red.oases.checkpoint.Utils.CommonUtils;
+import red.oases.checkpoint.Utils.ProgressUtils;
+
+import java.util.Random;
 
 @PermissionLevel(0)
 @DisableConsole
@@ -16,48 +19,27 @@ public class CommandJoin extends Command {
     }
 
     protected boolean execute() {
-        if (args.length < 2) {
-            LogUtils.send("参数不足：/cpt join <竞赛名称>", sender);
-            return true;
-        }
-
-        var cam = args[1];
-
-        if (!Campaign.isPresent(cam)) {
-            LogUtils.send("竞赛 " + cam + " 不存在。", sender);
-            return true;
-        }
-
         var p = (Player) sender;
 
-        var existingCampaigns = Campaign.get(p);
-
-        // config
-        if (existingCampaigns.size() > 2) {
-            LogUtils.send("加入的竞赛数量已达上限。", sender);
+        if (ProgressUtils.HasCampaignEnabled(p)) {
+            LogUtils.send("你已参赛。", sender);
             return true;
         }
 
-        for (var c : existingCampaigns) {
-            if (c.getName().equals(cam)) {
-                LogUtils.send("你已经加入了竞赛 " + c.getName() + "。", sender);
+        ProgressUtils.enableCampaignFor(p);
+        LogUtils.send("你已成功参赛。", sender);
+        var defaultCam = Config.getString("default-campaign-name");
+
+        if (defaultCam == null) {
+            var campaigns = CommonUtils.getCampaignNames().stream().toList();
+            if (campaigns.isEmpty()) {
+                LogUtils.send("暂无赛道可供选择。", sender);
                 return true;
             }
+            defaultCam = campaigns.get(new Random(campaigns.size()).nextInt());
         }
-
-        var campaign = new Campaign(cam);
-
-        if (campaign.isPrivate() || !campaign.isOpen()) {
-            LogUtils.send("无法加入此竞赛。", sender);
-            if (campaign.isPrivate()) LogUtils.send("此竞赛为私密。", sender);
-            else LogUtils.send("此竞赛处于关闭状态。", sender);
-            return true;
-        }
-
-        campaign.addPlayer(p);
-
-        LogUtils.send("你已成功加入竞赛 " + cam + "！", sender);
-        LogUtils.send("在正式开始之前，请键入 /cpt run <竞赛名> 准备。", sender);
+        LogUtils.send("默认为你选择的赛道为 " + defaultCam + "。", sender);
+        LogUtils.send("如需切换，请使用 /cpt switch 指令。", sender);
 
         return true;
     }
---
+++ src/main/java/red/oases/checkpoint/Commands/CommandSwitch.java
@@ -0,0 +1,56 @@
+package red.oases.checkpoint.Commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import red.oases.checkpoint.Extra.Annotations.DisableConsole;
+import red.oases.checkpoint.Extra.Annotations.PermissionLevel;
+import red.oases.checkpoint.Objects.Campaign;
+import red.oases.checkpoint.Utils.LogUtils;
+import red.oases.checkpoint.Utils.ProgressUtils;
+
+
+@DisableConsole
+@PermissionLevel(0)
+public class CommandSwitch extends Command {
+    public CommandSwitch(String[] args, CommandSender sender) {
+        super(args, sender);
+    }
+
+    @Override
+    protected boolean execute() {
+        if (args.length < 2) {
+            LogUtils.send("参数不足：/cpt switch <campaign>", sender);
+            return true;
+        }
+
+        var p = (Player) sender;
+
+        if (!ProgressUtils.HasCampaignEnabled(p)) {
+            LogUtils.send("你还没有参赛。", sender);
+            LogUtils.send("请使用 /cpt join 来参赛。", sender);
+            return true;
+        }
+
+        var cam = args[1];
+
+        if (!Campaign.isPresent(cam)) {
+            LogUtils.send(cam + " 对应的竞赛不存在", sender);
+            return true;
+        }
+
+        var currentCampaign = ProgressUtils.getRunningCampaign(p);
+        var campaign = new Campaign(cam);
+        if (currentCampaign != null) {
+            if (currentCampaign.getName().equals(cam)) {
+                LogUtils.send("你已经在竞赛 " + campaign.getName() + " 中了。", sender);
+                return true;
+            }
+        }
+
+        campaign.addPlayer(p);
+        ProgressUtils.refreshProgress(p);
+        ProgressUtils.setRunningCampaign(p, campaign);
+        LogUtils.send("已切换到 " + campaign.getName() + "。", sender);
+        return true;
+    }
+}
