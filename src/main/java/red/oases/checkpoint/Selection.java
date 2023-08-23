d46eb919f8434384b95dd6d0125c31582980bfdb
Subilan <suyuanliang1995@gmail.com>
2023-08-23 13:08:24 +0800

    feat: alternating selection

--- src/main/java/red/oases/checkpoint/Selection.java
+++ src/main/java/red/oases/checkpoint/Selection.java
@@ -1,11 +1,16 @@
 package red.oases.checkpoint;
 
 import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Selection {
     public static MemoryConfiguration memory = new MemoryConfiguration();
+    public static Map<String, Integer> states = new HashMap<>();
 
     /**
      * 查看当前的选择状态
@@ -13,12 +18,9 @@ public class Selection {
      * @param actionIdentifier 选择标识
      * @return 0 - 尚未选择；1 - 选择了一个点；2 - 选择了两个点
      */
-    public static int getState(String actionIdentifier) {
-        var list = memory.getIntegerList(actionIdentifier + ".value1");
-        if (list.isEmpty()) return 0;
-        list = memory.getIntegerList(actionIdentifier + ".value2");
-        if (list.isEmpty()) return 1;
-        return 2;
+    public static Integer getState(String actionIdentifier) {
+        var res = states.get(actionIdentifier);
+        return res == null ? 0 : res;
     }
 
     public static List<Integer> getData(String actionIdentifier, Integer stage) {
@@ -31,8 +33,16 @@ public class Selection {
 
     public static void create(String actionIdentifier, int x, int y, int z) {
         switch (getState(actionIdentifier)) {
-            case 0 -> memory.set(actionIdentifier + ".value1", List.of(x, y, z));
-            case 1, 2 -> memory.set(actionIdentifier + ".value2", List.of(x, y, z));
+            case 0, 2 -> {
+                memory.set(actionIdentifier + ".value1", List.of(x, y, z));
+                states.remove(actionIdentifier);
+                states.put(actionIdentifier, 1);
+            }
+            case 1 -> {
+                memory.set(actionIdentifier + ".value2", List.of(x, y, z));
+                states.remove(actionIdentifier);
+                states.put(actionIdentifier, 2);
+            }
         }
     }
 }
